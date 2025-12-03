3eb7bf19d304988f85390d287ac08813bfd3b7d5
Dr. Christoph Pospiech <pospiech-HD@t-online.de>
2025-12-03 15:33:27 +0100

    Move error handling to connect crate.

--- src/connect.rs
+++ src/connect.rs
@@ -1,5 +1,8 @@
 use sqlx::mysql::MySqlPool;
 
-pub async fn connect_to_database(database_url: &str) -> Result<MySqlPool, sqlx::Error> {
-    MySqlPool::connect(database_url).await
+pub async fn connect_to_database(database_url: &str) -> sqlx::Pool<sqlx::MySql> {
+    let pool = MySqlPool::connect(database_url)
+        .await
+        .expect("Failed to connect to database");
+    pool
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,8 @@ pub(crate) mod connect;
 async fn main() {
     // Connect to the database
     let default_url = "mysql://lmxtest:lmxtest@localhost/akonadi";
-    let pool = match connect::connect_to_database(default_url).await {
-        Ok(p) => p,
-        Err(e) => {
-            eprintln!("Failed to connect to database: {}", e);
-            return;
-        }
-    };
+    let pool = connect::connect_to_database(default_url).await;
+
     println!("Hello, world!");
 
     // Explicit disconnect
