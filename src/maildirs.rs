b4a540726210aac6c4cbf0636d25237c83abdad6
Dr. Christoph Pospiech <pospiech-HD@t-online.de>
2025-12-03 17:37:11 +0100

    Read and (test) print mail directories.

---
+++ src/maildirs.rs
@@ -0,0 +1,28 @@
+use sqlx::{FromRow, MySql, Pool};
+
+#[derive(Debug, FromRow)]
+#[allow(dead_code)]
+pub struct Collection {
+    pub id: i64,
+    pub remote_id: Option<String>,
+    pub remote_revision: Option<String>,
+    pub dir_name: String,
+    pub parent_id: Option<i64>,
+}
+
+pub async fn fetch_collections(pool: Pool<MySql>) -> Vec<Collection> {
+    sqlx::query_as::<_, Collection>(
+        "
+        SELECT `id`,
+               CONVERT(`remoteId`, CHAR)    AS `remote_id`,
+               CONVERT(`remoteRevision`, CHAR) AS `remote_revision`,
+               CONVERT(`name`, CHAR)      AS `dir_name`,
+               `parentId`    AS `parent_id`
+        FROM `collectiontable`
+        WHERE `resourceId` = 3
+        ",
+    )
+    .fetch_all(&pool)
+    .await
+    .expect("Failed to fetch collections")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 pub(crate) mod connect;
+pub(crate) mod maildirs;
 
 #[tokio::main]
 async fn main() {
     // Connect to the database
     let default_url = "mysql://lmxtest:lmxtest@localhost/akonadi";
     let pool = connect::connect_to_database(default_url).await;
+
+    let collections = maildirs::fetch_collections(pool.clone()).await;
+    for collection in collections {
+        println!("{:?}", collection);
+    }
 
     println!("Hello, world!");
 
