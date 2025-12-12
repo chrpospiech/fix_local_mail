6001d50e2606d0a3a9b139f9945b2d3d9b2d3287
Dr. Christoph Pospiech <pospiech-HD@t-online.de>
2025-12-12 11:05:36 +0100

    Some renaming and retructuring to prepare future changes.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use crate::connect::{connect_to_database, get_fallback_url};
-use crate::new_mails::find_new_mail_files;
-use crate::todoitems::fetch_todo_items;
 
 pub(crate) mod connect;
-pub(crate) mod maildirs;
-pub(crate) mod new_mails;
-pub(crate) mod target;
 pub(crate) mod todoitems;
 
 #[tokio::main]
@@ -14,28 +9,12 @@ async fn main() {
     let current_url: String = std::env::var("DATABASE_URL").unwrap_or_else(|_| get_fallback_url());
     let pool: sqlx::Pool<sqlx::MySql> = connect_to_database(&current_url).await;
 
-    // Fetch mail root directories
-    let root_paths: Vec<Option<String>> = maildirs::get_root_paths(pool.clone()).await;
-    // Find new mail files
-    let new_mail_list: Vec<String> = find_new_mail_files(root_paths).await;
-    // Fetch full paths of all mail directories
-    let full_paths: std::collections::HashMap<i64, String> =
-        maildirs::fetch_full_paths(pool.clone()).await;
-    // Fetch todo items corresponding to new mail files
-    let todo_items: Vec<todoitems::TodoItem> = fetch_todo_items(pool.clone(), new_mail_list).await;
+    let todo_items: Vec<todoitems::TodoItem> = todoitems::fetch_todo_items(pool.clone()).await;
 
     // Print fetched data
     for item in todo_items {
-        println!("{:?}", full_paths.get(&item.collection_id));
         println!("{:?}", item);
     }
-
-    let test_mail_file = "/home/cp/Mail/AltHendesse/cur/1540826869.R19.helios:2,S";
-    println!(
-        "Mail {} has timestamp: {}",
-        test_mail_file,
-        target::get_mail_time_stamp(test_mail_file)
-    );
 
     // Explicit disconnect
     pool.close().await;
--- src/todoitems.rs
+++ src/todoitems.rs
@@ -1,9 +1,14 @@
 use sqlx::QueryBuilder;
 use sqlx::{FromRow, MySql, Pool};
 
+pub(crate) mod maildirs;
+pub(crate) mod new_mails;
+pub(crate) mod source_path;
+pub(crate) mod target_path;
+
 #[derive(Debug, FromRow)]
 #[allow(dead_code)]
-pub struct TodoItem {
+pub struct TodoPimItem {
     pub id: i64,
     pub remote_id: Option<String>,
     pub collection_id: i64,
@@ -11,7 +16,7 @@ pub struct TodoItem {
     pub mime_type_id: i64,
 }
 
-pub async fn fetch_todo_items(pool: Pool<MySql>, mail_list: Vec<String>) -> Vec<TodoItem> {
+pub async fn fetch_todo_pim_items(pool: Pool<MySql>, mail_list: Vec<String>) -> Vec<TodoPimItem> {
     let mut query_builder = QueryBuilder::new(
         "SELECT `id`,
             CONVERT(`remoteId`, CHAR) AS `remote_id`,
@@ -44,4 +49,46 @@ pub async fn fetch_todo_items(pool: Pool<MySql>, mail_list: Vec<String>) -> Vec<
         .fetch_all(&pool)
         .await
         .expect("Failed to fetch mail todo items")
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct TodoItem {
+    pub id: i64,
+    pub remote_id: String,
+    pub dirty: bool,
+    pub source_path: String,
+    pub target_path: String,
+}
+
+pub async fn fetch_todo_items(pool: Pool<MySql>) -> Vec<TodoItem> {
+    // Fetch mail root directories
+    let root_paths: Vec<Option<String>> = maildirs::get_root_paths(pool.clone()).await;
+    // Find new mail files
+    let new_mail_list: Vec<String> = new_mails::find_new_mail_files(root_paths).await;
+    // Fetch full paths of all mail directories
+    let full_paths: std::collections::HashMap<i64, String> =
+        maildirs::fetch_full_paths(pool.clone()).await;
+    // Fetch todo items corresponding to new mail files
+    let todo_pim_items: Vec<TodoPimItem> = fetch_todo_pim_items(pool.clone(), new_mail_list).await;
+    let tbd = "tbd: ".to_string();
+    let test_mail_file = "/home/cp/Mail/AltHendesse/cur/1540826869.R19.helios:2,S";
+    println!(
+        "Test mail {} has timestamp: {}",
+        test_mail_file,
+        target_path::get_mail_time_stamp(test_mail_file)
+    );
+
+    let todo_items: Vec<TodoItem> = todo_pim_items
+        .into_iter()
+        .map(|item| TodoItem {
+            id: item.id,
+            remote_id: item.remote_id.unwrap_or_else(|| "tbd".to_string()),
+            dirty: item.dirty,
+            source_path: tbd.clone() + full_paths.get(&item.collection_id).unwrap_or(&tbd) + "/",
+            target_path: tbd.clone(),
+        })
+        .collect();
+
+    todo_items
 }
